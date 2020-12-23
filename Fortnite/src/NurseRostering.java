711f3d336d09dcd5175c057220e72b15e5e03e68
amedka <axelbrenac@hotmail.fr>
2020-12-23 16:35:41 +0100

    rendu NurseRostering.java
    comme pour le projet Fortnite, l'affichage de la solution peut être long.
    dans les deux cas cela est dut a la présence de variables intermédiaires.

--- Fortnite/src/NurseRostering.java
+++ Fortnite/src/NurseRostering.java
@@ -1,7 +1,7 @@
-
+package problems;
 import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solution;
 import org.chocosolver.solver.variables.IntVar;
-import org.chocosolver.util.ESat;
 import org.kohsuke.args4j.Option;
 
 public class NurseRostering extends AbstractProblem {
@@ -10,7 +10,7 @@ public class NurseRostering extends AbstractProblem {
 	private Data data = Data.inst1;
 
 	private int nb_nurses, nb_days, day_shift, lb_nightshift, ub_nightshift;
-	private IntVar[][] x;
+	private IntVar[][] NURSE;
 
 
 
@@ -27,29 +27,63 @@ public class NurseRostering extends AbstractProblem {
 
 		ub_nightshift = data.param(4);
 
-		int day = 1;
-		int night = 2;
-		int dayoff = 3;
+	//	int day = 1;
+	//	int night = 2;
+	//	int dayoff = 3;
 		
 		model = new Model();
 
+		NURSE =  model.intVarMatrix("NURSE", nb_nurses, nb_days, 1, 3);
 	//Variables
 		
 		// Constraint1: In each four day period a nurse must have at least one day off
+		for(int j=0; j<nb_nurses; j++) {
+			IntVar lim = model.intVar(0);
+			for (int i = 0; i < nb_days-3; i++ ) {
+				IntVar[] temps = new IntVar[] {NURSE[j][i], NURSE[j][i+1], NURSE[j][i+2], NURSE[j][i+3]};
+				
+				model.not(model.count(3, temps, lim )).post();
+			}
+		}
 
-		//TODO
 
 		// Constraint2: no nurse can be scheduled for 3 night shifts in a row
 
-		//TODO
+		for (int i = 0; i<nb_nurses; i++) {
+			for (int j=0; j<nb_days-2; j++) {
+				IntVar[] temps = {NURSE[i][j], NURSE[i][j+1], NURSE[i][j+2]};
+				model.not(model.count(2, temps,model.intVar(3))).post();
+			}
+		}
 
 		// Constraint3: no nurse can be scheduled for a day shift after a night shift
 
-		//TODO
+		for (int i = 0; i<nb_nurses; i++) {
+			for (int j=0; j<nb_days-1; j++) {
+				IntVar t = NURSE[j][i+1].mul(3).intVar();
+				model.arithm(NURSE[j][i], "+", t,"!=", 5).post();
+			}
+		}
 
 		// Constraint4: day shift size and min/max night shift size
 
-		//TODO
+		for (int i = 0; i<nb_days; i++) {
+			IntVar[] temps = new IntVar[nb_nurses];
+			for (int j=0; j<nb_nurses; j++) {
+				temps[j]= NURSE[j][i];
+			
+			}
+			model.not(model.count(2, temps, model.intVar(ub_nightshift+1))).post();
+			
+			model.count(2, temps,model.intVar(lb_nightshift) ).post();
+			
+			
+		    model.not(model.count(1, temps, model.intVar(day_shift+1))).post();
+		    
+			model.count(1, temps,model.intVar(day_shift) ).post();
+			
+			
+		}
 
 	}
 
@@ -61,8 +95,12 @@ public class NurseRostering extends AbstractProblem {
 	@Override
 	public void solve() {
 
-		model.getSolver().solve();
+		//model.getSolver().solve();
+		Solution solution = model.getSolver().findSolution();
 
+		if(solution != null){
+		    System.out.println(solution.toString());
+		}
 		//printSolution
 
 		model.getSolver().printStatistics();
